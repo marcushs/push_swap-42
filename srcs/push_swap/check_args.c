f5e47bcfd52e044cd77aaff5d0d08ac963a060c6
Hok sum Leung <hleung@z3r4p5.42lyon.fr>
2023-05-04 17:56:31 +0200

    found free pb, need to check others and malloc

--- srcs/push_swap/check_args.c
+++ srcs/push_swap/check_args.c
@@ -6,7 +6,7 @@
 /*   By: hleung <hleung@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 14:43:37 by hleung            #+#    #+#             */
-/*   Updated: 2023/04/03 13:19:17 by hleung           ###   ########lyon.fr   */
+/*   Updated: 2023/05/04 17:55:45 by hleung           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ static int	strs_to_arr(char *arg, long **arr)
 	long	num;
 
 	strs = ft_split(arg, ' ');
-	if (!strs || !check_char_strs(strs))
+	if (!strs || !check_char_strs(strs)) //free problem here and 
 		return (0);
 	i = -1;
 	while (strs[++i])
@@ -83,8 +83,10 @@ static void	args_to_arr(int argc, char **argv, long *arr)
 			arr++;
 		}
 		if (count > 1)
+		{
 			if (!strs_to_arr(argv[i], &arr))
-				free_error_exit((void **)&tmp, &free_normal_arr);
+				free_error_exit((void **)&tmp, &free_normal_arr);	
+		}
 	}
 }
 
